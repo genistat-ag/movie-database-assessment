1a417a800ad4067ba4fefaa805df87c702911d63
Mokhlesur Mahin <mokhlesur.rahman@pathao.com>
2023-02-17 05:05:02 +0600

    feat(reviews): movie review create validation set, movie avg rating update calculation

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ListCreateMovieAPIView.as_view(), name='get_post_movies'),
+    path('review/', views.ListCreateReviewAPIView.as_view(), name='get_post_review'),
     path('<str:pk>/', views.RetrieveUpdateDestroyMovieAPIView.as_view(), name='get_delete_update_movie'),
-    path('review',views.ListCreateReviewAPIView.as_view(),name='get_post_review')
 ]
--- movies/views.py
+++ movies/views.py
@@ -5,6 +5,10 @@ from .serializers import MovieSerializer,ReviewSerializer
 from .pagination import CustomPagination
 from .filters import MovieFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.exceptions import ValidationError
+from django.db.models import Sum
 
 # Removes permissions from views
 
@@ -34,6 +38,28 @@ class ListCreateReviewAPIView(ListCreateAPIView):
     queryset = Rating.objects.all()
     permission_classes = (IsAuthenticated,)
 
+    def validate_score(self, score):
+        if score>5 or score<1:
+            raise ValidationError('Rating range is 1-5 inclusive')
+
     def perform_create(self, serializer):
+        score = self.request.data.get('score')
+        movie_id = self.request.data.get('movie')
+        reviewer_id = self.request.user.id
+        self.validate_score(score)
+        is_rating_exist_for_same_movie = Rating.objects.filter(movie_id=movie_id, reviewer_id=reviewer_id).exists()
+        if is_rating_exist_for_same_movie:
+            raise ValidationError('You have already rated this movie')
+
         serializer.save(reviewer=self.request.user)
+        self.update_movie_rating_avg(movie_id)
+
+    def update_movie_rating_avg(self, movie_id):
+        movie_id = Movie.objects.get(id=movie_id)
+        ratings = Rating.objects.filter(movie=movie_id)
+        total_rating = ratings.aggregate(total = Sum("score"))
+        avg_rating = total_rating['total']/ratings.count()
+        movie_id.avg_rating = avg_rating
+        movie_id.save()
+
 
