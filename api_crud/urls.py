bbe521ed08a0b3ca96124f16eeae4f42fd3f69df
shoumitro-cse <shoumitro26@gmail.com>
2023-02-16 23:08:22 +0600

    api swagger & email or username auth fixed

--- api_crud/urls.py
+++ api_crud/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework import permissions
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
 # urls
@@ -9,4 +10,9 @@ urlpatterns = [
     path('api/v1/auth/', include('authentication.urls')),
     path('api/v1/movies/', include('movies.urls')),
     path('admin/', admin.site.urls),
+
+    # for API docs
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redocs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
