3c4bb6730b92648142f28c2a723624bcd160e7c3
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2023-02-16 20:44:53 +0600

    login

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,8 +1,97 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
+import importlib
+
+from django.contrib.auth.models import User, update_last_login
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.settings import api_settings
+from rest_framework import exceptions, serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 
+rule_package, user_eligible_for_login = api_settings.USER_AUTHENTICATION_RULE.rsplit('.', 1)
+login_rule = importlib.import_module(rule_package)
+
+class PasswordField(serializers.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('style', {})
+
+        kwargs['style']['input_type'] = 'password'
+        kwargs['write_only'] = True
+
+        super().__init__(*args, **kwargs)
+
+
+
+class CustomeTokenObtainSerializer(serializers.Serializer):
+    username_field = User.USERNAME_FIELD
+    email_field = User.EMAIL_FIELD
+
+    default_error_messages = {
+        'no_active_account': ('No active account found with the given credentials')
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'] = serializers.CharField(required=False, allow_null=True)
+        self.fields['email'] = serializers.CharField(required=False, allow_null=True)
+        self.fields['password'] = PasswordField()
+
+    def validate(self, attrs):
+
+        authenticate_kwargs = {
+            self.username_field: attrs[self.username_field],
+            'password': attrs['password'],
+        }
+
+        try:
+            authenticate_kwargs['request'] = self.context['request']
+        except KeyError:
+            pass
+
+        self.user = authenticate(**authenticate_kwargs)
+
+        if not getattr(login_rule, user_eligible_for_login)(self.user):
+            raise exceptions.AuthenticationFailed(
+                self.error_messages['no_active_account'],
+                'no_active_account',
+            )
+
+        return {}
+
+class CustomeTokenObtainPairSerializer(CustomeTokenObtainSerializer):
+    @classmethod
+    def get_token(cls, user):
+        return RefreshToken.for_user(user)
+
+    def validate(self, attrs):
+        submit_data = {
+
+        }
+        if attrs.__contains__("email"):
+            getusername=User.objects.get(email=attrs["email"])
+            submit_data = {
+                "username": getusername.username,
+                "password": attrs["password"]
+            }
+        else:
+            submit_data = {
+                "username": attrs["username"],
+                "password": attrs["password"]
+            }
+
+
+        data = super().validate(submit_data)
+
+        refresh = self.get_token(self.user)
+
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return data
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from authentication.views import RegisterView
+from authentication.views import RegisterView,CustomeTokenObtainPairView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', CustomeTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
 ]
