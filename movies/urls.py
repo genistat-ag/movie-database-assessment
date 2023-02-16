5e66e94a7e2d7553da9d755b4b8bf1c33eb1851a
Rafatlne <ashikrafat.lne@gmail.com>
2023-02-16 22:29:53 +0600

    Fixed: movies review url

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
+    path('review/', views.ListCreateReviewAPIView.as_view(), name='get_post_review'),
     path('', views.ListCreateMovieAPIView.as_view(), name='get_post_movies'),
     path('<str:pk>/', views.RetrieveUpdateDestroyMovieAPIView.as_view(), name='get_delete_update_movie'),
-    path('review',views.ListCreateReviewAPIView.as_view(),name='get_post_review')
 ]
