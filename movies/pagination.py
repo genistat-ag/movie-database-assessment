98f238bc3f92990d7cffcc35f352bf1cb46f9c1d
Rafatlne <ashikrafat.lne@gmail.com>
2023-02-17 11:04:18 +0600

    fixed: custom pagination doesn't work

--- movies/pagination.py
+++ movies/pagination.py
@@ -2,5 +2,5 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-    size = 10
-    size_query_param = 'page_size'
+    page_size = 10
+    page_size_query_param = 'page_size'
