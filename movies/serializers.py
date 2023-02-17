8fd615412810d9b3d0d4f0b0d75002c5e621fddc
antu7 <textmeantu@gmail.com>
2023-02-17 11:46:46 +0600

    user can only give 1 to 5 score for movies

--- movies/serializers.py
+++ movies/serializers.py
@@ -31,3 +31,8 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = ('id','movie','score','reviewer')
+    
+    def validate_score(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Score must be between 1 and 5.")
+        return value
