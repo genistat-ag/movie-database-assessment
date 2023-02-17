b8d947d683ed6748dfa3f51e0cf6cf114a4bf544
Rafatlne <ashikrafat.lne@gmail.com>
2023-02-17 11:21:15 +0600

    added: docstring for viewclass

--- movies/views.py
+++ movies/views.py
@@ -12,10 +12,18 @@ from .filters import MovieFilter
 from rest_framework.permissions import IsAuthenticated
 
 
-# Removes permissions from views
-
-
 class ListCreateMovieAPIView(ListCreateAPIView):
+    """
+     API view for listing and creating movies.
+
+     Attributes:
+    - serializer_class: A serializer class for the movie model.
+    - queryset: A queryset of all movies.
+    - pagination_class: A custom pagination class.
+    - filter_backends: A tuple of filter backends to be used.
+    - filterset_class: A filter class for the movie model.
+    - permission_classes: A tuple of permission classes for the view.
+     """
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
     pagination_class = CustomPagination
@@ -24,6 +32,16 @@ class ListCreateMovieAPIView(ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
+        """
+        Returns a filtered queryset of movies.
+
+        If the user is authenticated, the queryset will include movies that belong to the user and have the
+        is_inappropriate flag set to True, in addition to all movies that do not have this flag set. If the user is
+        a superuser, all movies will be included in the queryset.
+
+        Returns:
+        -  A filtered queryset of movies.
+        """
         user = self.request.user
         queryset = Movie.objects.filter(is_inappropriate=False)
 
@@ -43,6 +61,14 @@ class ListCreateMovieAPIView(ListCreateAPIView):
 
 
 class RetrieveUpdateDestroyMovieAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    API view for retrieving, updating, and deleting a single movie.
+
+    Attributes:
+    - serializer_class: A serializer class for the movie model.
+    - queryset: A queryset of all movies.
+    - permission_classes: A tuple of permission classes for the view.
+    """
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
@@ -54,6 +80,15 @@ class RetrieveUpdateDestroyMovieAPIView(RetrieveUpdateDestroyAPIView):
 
 
 class ListCreateReviewAPIView(ListCreateAPIView):
+    """
+    API view for listing and creating reviews.
+
+    Attributes:
+    - serializer_class: A serializer class for the review model.
+    - queryset: A queryset of all ratings.
+    - permission_classes: A tuple of permission classes for the view.
+    - pagination_class: A custom pagination class.
+    """
     serializer_class = ReviewSerializer
     queryset = Rating.objects.all()
     permission_classes = (IsAuthenticated,)
@@ -64,12 +99,31 @@ class ListCreateReviewAPIView(ListCreateAPIView):
 
 
 class RetrieveUpdateDestroyReviewAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    API view for retrieving, updating, and deleting a single review.
+
+    Attributes:
+    - serializer_class: A serializer class for the review model.
+    - queryset: A queryset of all ratings.
+    - permission_classes: A tuple of permission classes for the view.
+    """
     serializer_class = ReviewSerializer
     queryset = Rating.objects.all()
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
 
 
 class ReportViewSet(viewsets.ModelViewSet):
+    """
+    API view for managing reports.
+
+    Attributes:
+    - serializer_class: A serializer class for the report model.
+    - queryset: A queryset of all reports.
+    - http_method_names: A list of HTTP methods allowed for the view.
+    - permission_classes: A tuple of permission classes for the view.
+    - pagination_class: A custom pagination class.
+    """
+
     serializer_class = ReportSerializer
     queryset = Report.objects.all()
     http_method_names = ['get', 'post', 'patch']
@@ -77,6 +131,12 @@ class ReportViewSet(viewsets.ModelViewSet):
     pagination_class = CustomPagination
 
     def get_queryset(self):
+        """
+        Returns a queryset of all reports that the user is allowed to see.
+
+        Returns:
+        - A queryset of reports.
+        """
         user = self.request.user
         if user.is_superuser:
             return Report.objects.all()
@@ -88,6 +148,16 @@ class ReportViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['patch'], url_path='mark-inappropriate')
     def mark_as_inappropriate(self, request, pk=None):
+        """
+        Marks a report as inappropriate and hides the associated movie.
+
+        Args:
+        - request: The HTTP request object.
+        - pk: The primary key of the report.
+
+        Returns:
+        - A response with the serialized report data.
+        """
         report = self.get_object()
         if report.state != State.UNRESOLVED:
             return Response({"detail": "Report has already been resolved"}, status=status.HTTP_400_BAD_REQUEST)
@@ -105,6 +175,16 @@ class ReportViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['patch'], url_path='reject')
     def reject_report(self, request, pk=None):
+        """
+        Rejects a report.
+
+        Args:
+        - request: The HTTP request object.
+        - pk: The primary key of the report.
+
+        Returns:
+        - A response with the serialized report data.
+        """
         report = self.get_object()
         if report.state != State.UNRESOLVED:
             return Response({"detail": "Report has already been resolved"}, status=status.HTTP_400_BAD_REQUEST)
@@ -113,6 +193,16 @@ class ReportViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['patch'], url_path='revert')
     def revert_report(self, request, pk=None):
+        """
+        Reverts a report to the UNRESOLVED state.
+
+        Args:
+        - request: The HTTP request object.
+        - pk: The primary key of the report.
+
+        Returns:
+        - A response with the serialized report data.
+        """
         report = self.get_object()
         if report.state == State.UNRESOLVED:
             return Response({"detail": "Report hasn't been resolve yet"}, status=status.HTTP_400_BAD_REQUEST)
