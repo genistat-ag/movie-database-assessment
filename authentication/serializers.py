0bd3730ebb90a322bb74bb66c92354d841db8c99
Salekeen <nawazsalekeennayeem@gmail.com>
2023-03-03 13:02:36 +0600

    auth almost done

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -2,38 +2,82 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+        data["access_token"] = str(refresh.access_token)
+        data["refresh_token"] = str(refresh)
+        return data
+
+
+class LoginSerializer(serializers.Serializer):
+    username_or_email = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    access_token = serializers.CharField(read_only=True)
+    refresh_token = serializers.CharField(read_only=True)
+
+    def validate(self, attrs):
+        username_or_email = attrs.get("username_or_email")
+        password = attrs.get("password")
+
+        if username_or_email and password:
+            user = authenticate(
+                request=self.context.get("request"),
+                username=username_or_email,
+                email=username_or_email,
+                password=password,
+            )
+            if not user:
+                raise serializers.ValidationError("Invalid login credentials")
+        else:
+            raise serializers.ValidationError(
+                'Must include "username_or_email" and "password".'
+            )
+
+        refresh = RefreshToken.for_user(user)
+        attrs["access_token"] = str(refresh.access_token)
+        attrs["refresh_token"] = str(refresh)
+        attrs["user"] = user
+        return attrs
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
+        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    username = serializers.CharField(
+        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
     )
 
     password = serializers.CharField(
-        read_only=False, required=True, validators=[validate_password],
-        style={'input_type': 'password', 'placeholder': 'Password'}
-    )
-    password2 = serializers.CharField(
-        read_only=False, required=True, style={'input_type': 'password', 'placeholder': 'Password'}
+        read_only=False,
+        required=True,
+        validators=[validate_password],
+        style={"input_type": "password", "placeholder": "Password"},
     )
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
-
-    def validate(self, attrs):
-        if attrs['password'] == attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
+        fields = (
+            "username",
+            "password",
+            "email",
+            "first_name",
+            "last_name",
+        )
 
     def create(self, validated_data):
-        del validated_data['password2']
+        # del validated_data["password2"]
         user = User.objects.create(**validated_data)
 
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data["password"])
         user.save()
 
         return user
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,44 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework.permissions import IsAdminUser, AllowAny
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import generics, status
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    permission_classes = (IsAdminUser,)
+    permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username", None)
+        email = request.data.get("email", None)
+        password = request.data.get("password", None)
+
+        if email is not None:
+            user = authenticate(request, email=email, password=password)
+        else:
+            user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            return Response(
+                {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+        )
