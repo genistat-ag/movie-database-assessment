97e8230c499eb4e1cf872f17e651a75de3b9d94a
Noyon <noyon13nil@gmail.com>
2023-04-10 03:38:37 +0600

    model updated

--- movieAssessment/mainapp/models.py
+++ movieAssessment/mainapp/models.py
@@ -29,18 +29,73 @@ class Users(AbstractBaseUser):
     def __str__(self):
         return '%s' % (self.full_name)
 
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
-
-
     class Meta:
         db_table = 'users'
         verbose_name = "User"
         verbose_name_plural = "Users" 
+
+
+class Movie(models.Model):
+    title             = models.CharField(max_length=100)
+    description 	  = models.TextField(max_length=3000)
+    title_upload_date = models.DateTimeField(auto_now=True)
+    movie_cover 	  = models.FileField(upload_to='movie_covers/')
+    uploaded_by		  = models.ForeignKey(Users, related_name="movie_uploaded_by", on_delete=models.CASCADE, null=True, blank=True, default=None)
+    updated_at 		  = models.DateTimeField(auto_now=True)
+    updated_by 		  = models.ForeignKey(Users, related_name="movie_updated_by", on_delete=models.CASCADE, null=True, blank=True, default=None)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'movies'
+        verbose_name = "Movie"
+        verbose_name_plural = "Movies" 
+    
+
+class Rating(models.Model):
+    author 			= models.ForeignKey(Users, related_name="rating_author", on_delete=models.CASCADE, null=True, blank=True, default=None)
+    rating_date		= models.DateTimeField(auto_now=True)
+    rate_choices = (
+        (1,1),
+        (2,2),
+        (3,3),
+        (4,4),
+        (5,5)
+    )
+    stars 			= models.IntegerField(choices=rate_choices)
+    comment 		= models.TextField(max_length=4000)
+    movie 			= models.ForeignKey(Movie, on_delete=models.CASCADE)
+    updated_at 		= models.DateTimeField(auto_now=True)
+    updated_by 		= models.ForeignKey(Users, related_name="rating_updated_by", on_delete=models.CASCADE, null=True, blank=True, default=None)
+
+    def __str__(self):
+        return self.movie.title
+
+    class Meta:
+        db_table = 'ratings'
+        verbose_name = "Rating"
+        verbose_name_plural = "Ratings"
+
+
+class Report(models.Model):
+    author 			= models.ForeignKey(Users, related_name="reporting_author", on_delete=models.CASCADE, null=True, blank=True, default=None)
+    reporting_date  = models.DateTimeField(auto_now=True)
+    comment 		= models.TextField(max_length=4000)
+    report_status_choices = (
+        ('Unresolved','Unresolved'),
+        ('Inappropriate','Inappropriate'),
+        ('Rejected','Rejected'),
+    )
+    stars 			= models.CharField(max_length=20, choices=report_status_choices)
+    movie 			= models.ForeignKey(Movie, on_delete=models.CASCADE)
+    updated_at 		= models.DateTimeField(auto_now=True)
+    updated_by 		= models.ForeignKey(Users, related_name="reporting_updated_by", on_delete=models.CASCADE, null=True, blank=True, default=None)
+
+    def __str__(self):
+        return self.movie.title
+
+    class Meta:
+        db_table = 'reports'
+        verbose_name = "Report"
+        verbose_name_plural = "reports"
